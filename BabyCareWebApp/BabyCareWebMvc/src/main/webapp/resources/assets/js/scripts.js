34542a3fb7c9d46a3b845019c783bea99cb2efec
georve <georve@gmail.com>
2016-01-03 17:19:20 -0430

    se agrega e nuevo template de la pagina dashboard

--- BabyCareWebApp/BabyCareWebMvc/src/main/webapp/resources/assets/js/scripts.js
+++ BabyCareWebApp/BabyCareWebMvc/src/main/webapp/resources/assets/js/scripts.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function() {
     /*
         Fullscreen background
     */
-    $.backstretch("assets/img/backgrounds/1.jpg");
+    $.backstretch("babycare/resources/assets/img/backgrounds/1.jpg");
     
     /*
         Form validation
